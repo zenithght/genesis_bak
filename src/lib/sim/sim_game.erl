7ad4eb1892fd23b79ace7befe6187ee9cfbf5d40
HpyHacking <crazyflapjack@gmail.com>
2012-03-09 12:43:56 +0800

    構建一個模擬兩位玩家登陸的環境

--- src/lib/sim/sim_game.erl
+++ src/lib/sim/sim_game.erl
@@ -8,5 +8,35 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-define(JACK, jack).
+-define(JACK_ID, "jack").
+-define(TOMMY, tommy).
+-define(TOMMY_ID, "tommy").
+
 join_empty_game_test() ->
-  ok.
+  run_by_login_two_players(fun() ->
+        ok
+    end).
+
+run_by_login_two_players(Fun) ->
+  schema:init(),
+  mnesia:dirty_write(sim_client:player(?JACK)),
+  mnesia:dirty_write(sim_client:player(?TOMMY)),
+
+  sim_client:kill_games(),
+
+  %% login Jack & Tommy
+  lists:map(fun({Key, Id}) ->
+        sim_client:kill_player(Id),
+        sim_client:start(Key),
+        sim_client:send(Key, #login{usr = list_to_binary(Id), pass = <<?DEF_PWD>>}),
+        ?assertMatch(#player_info{}, sim_client:head(Key)),
+        ?assertMatch(#balance{}, sim_client:head(Key))
+    end, [{?JACK, ?JACK_ID}, {?TOMMY, ?TOMMY_ID}]),
+
+  Fun().
+
+start_game() ->
+  Limit = #limit{min = 100, max = 400, small = 5, big = 10},
+  Conf = #tab_game_config{module = game, mods = [{wait_players, []}], limit = Limit, seat_count = 9, start_delay = 3000, required = 2, timeout = 1000, max = 2},
+  game:start(Conf).
