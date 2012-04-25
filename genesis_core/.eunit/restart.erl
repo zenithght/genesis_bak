8f57d12747d19b550746409759cf47e13f9b6bd4
HpyHacking <crazyflapjack@gmail.com>
2012-04-25 16:19:12 +0800

    delete some unless files

--- genesis_core/.eunit/restart.erl
+++
@@ -1,5 +0,0 @@
--module(restart).
--export([start/2]).
-
-start([], Ctx) ->
-  {goto, top, Ctx}.
--- genesis_core/.eunit/rig.erl
+++
@@ -1,8 +0,0 @@
--module(rig).
--export([start/2]).
-
--include("game.hrl").
-
-start([Cards], Ctx) when is_list(Cards) ->
-  {stop, Ctx#texas{deck = deck:new(Cards)}};
-start(_, Ctx) -> {stop, Ctx}.
